99fe90d2105ca1b14a3f5d219d6458952e101ad5
fsm2005 <143094862+fsm2005@users.noreply.github.com>
2024-09-09 11:56:53 -0500

    Add files via upload

---
+++ tarea2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string unidades[10] = { "", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve" };
+string decenas[10] = { "", "diez", "veinte", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa" };
+string problema[10] = { "diez", "once", "doce", "trece", "catorce", "quince", "dieciséis", "diecisiete", "dieciocho", "diecinueve" };
+string centenas[10] = { "", "ciento", "doscientos", "trescientos", "cuatrocientos", "quinientos", "seiscientos", "setecientos", "ochocientos", "novecientos" };
+
+string transmutar(int num);
+
+int main() {
+    int valor;
+    cout << "Ingrese un numero: ";
+    cin >> valor;
+    cout << "El número en letras es: " << transmutar(valor) << endl;
+}
+
+string transmutar(int num) {
+    if (num == 0) return "Cero";
+
+    string resul;
+
+    // Manejo de centenas de mil (100000 a 999999)
+    if (num >= 100000) {
+        int centenasMil = num / 100000;
+        resul += centenas[centenasMil] + " ";
+        num %= 100000;
+    }
+    if (num >= 21000 && num < 22000) { // Manejo de números entre 21000 y 21999
+        resul += "Veintiun Mil ";
+        num %= 1000;
+    }
+    // Manejo de decenas de mil (20000 a 99999)
+    if (num >= 20000) {
+        int decenasMil = num / 10000;
+        int unidadesMil = (num / 1000) % 10;
+
+        if (decenasMil == 2 && unidadesMil > 0) { // Caso especial para 21, 22, 23, ..., 29 mil
+            resul += "Veinti" + unidades[unidadesMil] + " Mil ";
+            num %= 1000;
+        }
+        else {
+            resul += decenas[decenasMil] + " y ";
+            num %= 10000;
+        }
+    }
+    else if (num >= 10000) { // Manejo de números entre 10000 y 19999
+        int miles = (num / 1000) % 10;
+        resul += problema[miles] + " Mil ";
+        num %= 1000;
+    }
+
+    // Manejo de unidades de mil (1000 a 9999)
+    if (num >= 1000) {
+        int unidadesMil = num / 1000;
+        if (unidadesMil == 1 && num % 1000 == 0) {
+            return resul + "Mil";
+        }
+        else if (unidadesMil == 1) {
+            resul += "Mil ";
+        }
+        else {
+            resul += unidades[unidadesMil] + " Mil ";
+        }
+        num %= 1000;
+    }
+ 
+    // Manejo de centenas (100 a 999)
+    if (num >= 100) {
+        int centegan = num / 100;
+        resul += centenas[centegan] + "  ";
+        num %= 100;
+    }
+
+    // Manejo de decenas (20 a 99)
+    if (num >= 20) {
+        if (num / 10 == 2 && num % 10 > 0) { // Caso especial para 21, 22, 23, ..., 29
+            resul += "Veinti" + unidades[num % 10];
+            num = 0;
+        }
+        else {
+            resul += decenas[num / 10] + " y ";
+            num %= 10;
+        }
+    }
+    else if (num >= 10) { // Manejo de números entre 10 y 19
+        return resul + problema[num - 10];
+    }
+
+    // Manejo de unidades (1 a 9)
+    resul += unidades[num];
+
+
+
+    return resul;
+}
