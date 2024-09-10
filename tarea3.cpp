2fd53598d0fe04cdd887fbe00cc4d025e5cdce5d
fsm2005 <143094862+fsm2005@users.noreply.github.com>
2024-09-10 07:31:13 -0500

    Add files via upload

--- tarea3.cpp
+++ tarea3.cpp
@@ -10,34 +10,32 @@ void imprimirArray(int arr[], int tam) {
 }
 
 void ordenarArray(int* arr, int tam) {
-    int* puntero1 = arr; 
-    int* puntero2 = arr + tam / 2;
+    int* puntero1 = arr; int* puntero2 = arr + tam / 2; int* finalPuntero1 = arr + tam / 2; int* finalArr = arr + tam;
 
-    for (int i = 0; i < tam / 2; ++i) {
-        for (int j = 0; j < tam / 2; ++j) {
-            if (*(puntero1 + j) > *(puntero2 + j)) {
-                int temp = *(puntero1 + j);
-                *(puntero1 + j) = *(puntero2 + j);
-                *(puntero2 + j) = temp;
-            }
+ 
+    for (int* p1 = puntero1, *p2 = puntero2; p1 < finalPuntero1; ++p1, ++p2) {
+        if (*p1 > *p2) {
+            int temp = *p1;
+            *p1 = *p2;
+            *p2 = temp;
         }
     }
 
-    for (int i = 0; i < tam - 1; ++i) {
-        for (int j = 0; j < tam - i - 1; ++j) {
-            if (*(arr + j) > *(arr + j + 1)) {
-                int temp = *(arr + j);
-                *(arr + j) = *(arr + j + 1);
-                *(arr + j + 1) = temp;
+    for (int* p1 = arr; p1 < finalArr - 1; ++p1) {
+        for (int* p2 = p1 + 1; p2 < finalArr; ++p2) {
+            if (*p1 > *p2) {
+                int temp = *p1;
+                *p1 = *p2;
+                *p2 = temp;
             }
         }
     }
 }
 
 int main() {
-    
-    int arr[] = { 1, 4, 8, 9, 2, 3, 19, 5 };
-    int tam= sizeof(arr) / sizeof(arr[0]);
+
+    int arr[] = { 2, 6, 8,12,20,1,3,7,11,15 };
+    int tam = sizeof(arr) / sizeof(arr[0]);
 
     cout << "Array original: ";
     imprimirArray(arr, tam);
