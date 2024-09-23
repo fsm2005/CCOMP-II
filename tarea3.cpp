1c664c97edf5956304116025f537eafcb92f4730
fsm2005 <143094862+fsm2005@users.noreply.github.com>
2024-09-23 15:07:08 -0500

    tarea3.cpp

--- tarea3.cpp
+++ tarea3.cpp
@@ -2,43 +2,13 @@
 
 using namespace std;
 
-void imprimirArray(int arr[]) {
-    
-    for (int i = 0; arr[i] != 0; ++i) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
+void merge(int* arr);
 
-void merge(int* arr) {
-    int* puntero1 = arr;
-
-    int* temp = arr;
-
-    for (;*temp;temp++) {}
-  
-    int* puntero2 = puntero1 + (temp - puntero1) / 2;
-
-    int* final = puntero2 + (puntero2 - puntero1);  
-
-    
-    while ((puntero1 < puntero2) && (puntero2 > final)) {
-        if (*puntero1 > *puntero2) {
-            int temp = *puntero1;
-            *puntero1 = *puntero2;
-            *puntero2 = temp;
-            
-        }
-        else {
-            puntero1++;  
-            puntero2++;  
-        }
-    }
-}
+void imprimirArray(int arr[]);
 
 int main() {
-    int arr[] = { 2, 6, 8, 20, 1, 3, 7, 11 ,
-        0}; 
+    int arr[] = { 2, 16, 34 ,88 ,1, 13 ,55, 99,
+        0 }; 
     cout << "Array original: ";
     imprimirArray(arr);
 
@@ -48,4 +18,43 @@ int main() {
     imprimirArray(arr);
 
     return 0;
+}
+void imprimirArray(int arr[]) {
+    for (int i = 0; arr[i] != 0; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void merge(int* arr) {
+
+    for (int i = 0; i < 7; i++) {
+        int* puntero1 = arr;
+
+        int* temp = arr;
+        for (; *temp; temp++) {}
+
+        int* puntero2 = puntero1 + (temp - puntero1) / 2;
+
+        int* final = puntero2 + (puntero2 - puntero1);
+
+        while ((puntero1 < puntero2) && (puntero2 < final)) {
+            int* r = puntero1;
+
+            while (r < final) {
+                puntero1 = r;
+                puntero2 = r + 1;
+
+                if (*puntero1 > *puntero2 && puntero2 < final) {
+                    int temp = *puntero1;
+                    *puntero1 = *puntero2;
+                    *puntero2 = temp;
+                }
+                r++;
+            }
+
+            puntero1++;
+            puntero2++;
+        }
+    }
 }
