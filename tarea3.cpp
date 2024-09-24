77c3fb124118b02b2b816a36c188d13ed64a1bbe
fsm2005 <143094862+fsm2005@users.noreply.github.com>
2024-09-24 07:33:49 -0500

    tarea3.cpp

--- tarea3.cpp
+++ tarea3.cpp
@@ -8,11 +8,12 @@ void imprimirArray(int arr[]);
 
 int main() {
     int arr[] = { 2, 16, 34 ,88 ,1, 13 ,55, 99,
-        0 }; 
+        0 };
     cout << "Array original: ";
     imprimirArray(arr);
 
     merge(arr);
+    //merge(arr,arr+5);
 
     cout << "Array ordenado: ";
     imprimirArray(arr);
@@ -26,6 +27,7 @@ void imprimirArray(int arr[]) {
     cout << endl;
 }
 
+//void merge(int* ini. int *med) 
 void merge(int* arr) {
 
     for (int i = 0; i < 7; i++) {
@@ -37,6 +39,8 @@ void merge(int* arr) {
         int* puntero2 = puntero1 + (temp - puntero1) / 2;
 
         int* final = puntero2 + (puntero2 - puntero1);
+
+        // final = med + ((med-ini)-1)
 
         while ((puntero1 < puntero2) && (puntero2 < final)) {
             int* r = puntero1;
@@ -52,7 +56,6 @@ void merge(int* arr) {
                 }
                 r++;
             }
-
             puntero1++;
             puntero2++;
         }
