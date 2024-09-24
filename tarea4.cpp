df4ea62d7b28aed5db460120f96fcb1c3423e86e
fsm2005 <143094862+fsm2005@users.noreply.github.com>
2024-09-23 23:51:47 -0500

    tarea4.cpp

--- tarea4.cpp
+++ tarea4.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 
 using namespace std;
-
-void merge_par_impar (int* arr);
-
+void  split(int* arr);
 void imprimirArray(int arr[]);
 
 int main() {
     int arr[] = { 2, 6, 4 ,8 ,1, 7 ,5, 9,
-        0 }; 
+        0 };
     cout << "Array original: ";
     imprimirArray(arr);
 
-    merge(arr);
+    split(arr);
 
     cout << "Array ordenado: ";
     imprimirArray(arr);
@@ -25,20 +23,18 @@ void imprimirArray(int arr[]) {
     }
     cout << endl;
 }
+void  split(int* arr) {
 
-void merge_par_impar(int* arr) {
-
-    for (int i = 0; i < 7; i++) {
         int* puntero1 = arr;
 
         int* temp = arr;
         for (; *temp; temp++) {}
-  
+
         int* puntero2 = puntero1 + (temp - puntero1) / 2;
 
         for (int* p1 = puntero1; p1 < puntero2 - 1; ++p1) {
             for (int* p2 = p1 + 1; p2 < puntero2; ++p2) {
-                if (*p1 < *p2) {  
+                if (*p1 < *p2) {
                     int temp = *p1;
                     *p1 = *p2;
                     *p2 = temp;
@@ -47,12 +43,11 @@ void merge_par_impar(int* arr) {
         }
         for (int* p1 = puntero2; *p1 != 0; ++p1) {
             for (int* p2 = p1 + 1; *p2 != 0; ++p2) {
-                if (*p1 < *p2) {  
+                if (*p1 < *p2) {
                     int temp = *p1;
                     *p1 = *p2;
                     *p2 = temp;
                 }
             }
         }
-    }
 }
