8b77220738a55cd32e7bc9b2793064c77ec07920
fsm2005 <143094862+fsm2005@users.noreply.github.com>
2024-09-30 20:12:43 -0500

    tarea5.cpp

--- tarea5.cpp
+++ tarea5.cpp
@@ -1,31 +1,25 @@
 #include <iostream>
 
 using namespace std;
-
-void ordenar(int* arr, int n);
-
-void intercambiar(int* a, int* b);
+void ordenar(int* arr, int*medio);
+void imprimirArray(int arr[], int size);
 
 int main() {
     int arr[] = { 51, 3, 28, 35, 4, 2, 7, 23, 15,10};
-    
-
+    int size = sizeof(arr) / sizeof(arr[0]);
     cout << "Original: ";
-    for (int i = 0; i < ta; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-
-   
+    imprimirArray(arr, size); 
     ordenar(arr, arr+5);
-
     cout << "Ordenado: ";
-    for (int i = 0; i < ta; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-
+    imprimirArray(arr, size); 
     return 0;
 }
-
+void imprimirArray(int arr[], int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 void ordenar(int* arr, int* medio) {
     bool cambio = true;
     int* ini = arr;
@@ -36,31 +30,29 @@ void ordenar(int* arr, int* medio) {
 
         for (int* ptr = ini; ptr < fin; ++ptr) {
             if (*ptr > *(ptr + 1)) {
-                intercambiar(ptr, ptr + 1);  
+                int temp = *(ptr+1);
+                *(ptr+1) = *ptr;
+                *ptr = temp;
+
                 cambio = true;
             }
         }
-
+        
         if (!cambio)
             break;
-
         --fin;
-
         cambio = false;
 
         for (int* ptr = fin; ptr > ini; --ptr) {
             if (*ptr < *(ptr - 1)) {
-                intercambiar(ptr, ptr - 1);  
+                int temp = *(ptr-1);
+                *(ptr-1) = *ptr;
+                *ptr = temp;
+                
+                
                 cambio = true;
             }
         }
-
         ++ini;
     }
-}
-
-void intercambiar(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
 }
