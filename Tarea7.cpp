168d073eda852f7dcb8b7fea74659b209079f3a0
Gamblok <143094862+fsm2005@users.noreply.github.com>
2024-11-05 08:12:05 -0500

    Update Tarea7.cpp

--- Tarea7.cpp
+++ Tarea7.cpp
@@ -28,41 +28,41 @@ void Print(nodo<T>* head) {
 }
 
 template <class T>
-void merge(nodo<T>* A, nodo<T>* B) {
-        nodo<T>* ini = nullptr;
-        nodo<T>* fin = nullptr;
-
+void merge(nodo<T>* &A, nodo<T>* &B) {
+    nodo<T>* ini = nullptr;
+    nodo<T>* val = nullptr;
+    if (A->valor < B->valor) {
+        ini = A;
+        A = A->next;
+    }
+    else {
+        ini = B;
+        B = B->next;
+    }
+    val = ini;
+    for (; A != nullptr && B != nullptr; val = val->next) {
         if (A->valor < B->valor) {
-            ini = A;
+            val->next = A;
             A = A->next;
         }
         else {
-            ini = B;
+            val->next = B;
             B = B->next;
         }
-        fin = ini;
-        for (; A != nullptr && B != nullptr; fin = fin->next) {
-            if (A->valor < B->valor) {
-                fin->next = A;
-                A = A->next;
-            }
-            else {
-                fin->next = B;
-                B = B->next;
-            }
-        }
-        if (A != nullptr) {
-            fin->next = A;
-        }
-        else {
-            fin->next = B;
-        }
-        A = ini;
     }
+    if (A != nullptr) {
+        val->next = A;
+    }
+    else {
+        val->next = B;
+    }
+    A = ini;
+    B = nullptr;
+}
 
 int main() {
     int A[] = { 1,2,3,5,6,13 };
-    int B[] = { 12,13,20};
+    int B[] = { 12,13,20 };
     nodo<int>* LEA;
     nodo<int>* LEB;
     ArraytoLe(A, A + (sizeof(A) / sizeof(A[0]) - 1), LEA);
@@ -71,4 +71,5 @@ int main() {
     Print(LEB);
     merge(LEA, LEB);
     Print(LEA);
+    Print(LEB);
 }
